3c08987014896f4db5af578e122e5ce8c6d41861
PRIYANSHU RANJAN <148432560+Priyanshu1210R@users.noreply.github.com>
2026-06-27 14:30:18 +0530

    Enhance password hashing with error handling
    Added error handling to password hashing process.

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -26,10 +26,28 @@ const UserSchema = new mongoose.Schema({
 
 // Hash password before saving
 UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+
+    try {
+
+        if (!this.isModified("password")) {
+            return next();
+        }
+
+        const salt = await bcrypt.genSalt(10);
+
+        this.password = await bcrypt.hash(
+            this.password,
+            salt
+        );
+
+        next();
+
+    } catch (err) {
+
+        next(err);
+
+    }
+
 });
 
 // Compare plain password with hashed
